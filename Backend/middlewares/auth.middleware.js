4cfb30a1e07411eac1a1a1a0c7d637c0b7b66b75
Saurav Kumar <sauraviiitk18@gmail.com>
2026-06-02 17:10:55 +0530

    Fix auth middleware

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -4,34 +4,54 @@ const jwt=require('jsonwebtoken');
 const userModel = require('../models/user.model');
 const BlackListTokenModel = require('../models/Blacklisttoken.modle');
 
-async function auth (req,res,next){
-    const token=req.headers.authorization?.split(" ")[1]||req.cookies.token;
-    if(!token){
-        return res.json({
-            message:"access denied , no token provided"
-        })
-    };
-    const isblacklistedtoken=await BlackListTokenModel.findOne({token:token})
-    if(isblacklistedtoken){
-        return res.status(400).json({
-            message:"invalid token"
-        })
+async function auth(req, res, next) {
+
+    const token =
+        req.headers.authorization?.split(" ")[1] ||
+        req.cookies?.token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Access denied, no token provided"
+        });
     }
-    try{
-                // decode the token 
-                const decoded=jwt.verify(token,process.env.JWT_SECRET);
-                const user=await userModel.findById(decoded._id);
-                if(!user){
-                    return res.status(401).json({   
-                        message:"invalid token"
-                    })}
-                    req.user=user;
-              return   next();
+
+    const isblacklistedtoken =
+        await BlackListTokenModel.findOne({ token });
+
+    if (isblacklistedtoken) {
+        return res.status(401).json({
+            message: "Invalid token"
+        });
     }
-    catch(err){
-        return res.status(400).json({
-            message:"invalid token"
-        })
+
+    try {
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        const user = await userModel.findById(
+            decoded._id
+        );
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid token"
+            });
+        }
+
+        req.user = user;
+
+        next();
+
+    } catch (err) {
+
+        return res.status(401).json({
+            message: "Invalid token"
+        });
+
     }
 }
 module.exports=auth;
